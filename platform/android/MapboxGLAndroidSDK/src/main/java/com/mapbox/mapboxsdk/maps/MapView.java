9fa98d2cff558116034f578a0eb778a41a55470c
Tobrun <tobrun.van.nuland@gmail.com>
2016-10-12 11:31:50 +0200

    [android] - fixup MyLocationView heading
    [android] - convert angles to radiants in jni, correct follow animation for MyLocationView
    
    do not sanitise input when recreating from other camera object, input is already sanitised before
    
    cleanup mylocationview, made animation bit more smooth, improved UX when switching between tracking modes
    
    cleanup CameraUpdateFactory, hide internally used api.
    
    only invalidate position if required, remove outdated comments
    
    correct camera position values
    
    made zoom activity a bit more interesting
    
    made dynamic marker activity a bit more interesting
    
    FIXUP

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/maps/MapView.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/maps/MapView.java
@@ -1316,7 +1316,7 @@ public class MapView extends FrameLayout {
             return;
         }
         nativeMapView.cancelTransitions();
-        nativeMapView.jumpTo(Math.toRadians(-bearing), center, Math.toRadians(pitch), zoom);
+        nativeMapView.jumpTo(bearing, center, pitch, zoom);
     }
 
     void easeTo(double bearing, LatLng center, long duration, double pitch, double zoom, boolean easingInterpolator, @Nullable final MapboxMap.CancelableCallback cancelableCallback) {
@@ -1340,7 +1340,7 @@ public class MapView extends FrameLayout {
             });
         }
 
-        nativeMapView.easeTo(Math.toRadians(-bearing), center, duration, Math.toRadians(pitch), zoom, easingInterpolator);
+        nativeMapView.easeTo(bearing, center, duration, pitch, zoom, easingInterpolator);
     }
 
     void flyTo(double bearing, LatLng center, long duration, double pitch, double zoom, @Nullable final MapboxMap.CancelableCallback cancelableCallback) {
@@ -1364,7 +1364,7 @@ public class MapView extends FrameLayout {
             });
         }
 
-        nativeMapView.flyTo(Math.toRadians(-bearing), center, duration, Math.toRadians(pitch), zoom);
+        nativeMapView.flyTo(bearing, center, duration, pitch, zoom);
     }
 
     private void adjustTopOffsetPixels() {
